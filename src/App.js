84be34ecfcd749f0d7d4a8c2868375dca64f6adc
Dmity-kl <i6169696@gmail.com>
2022-08-23 23:47:25 +0300

    Add component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import './scss/app.scss';
+import {Header} from "./components/Header/Header";
+import {Categories} from "./components/Categories/Categories";
+import {Sort} from "./components/Sort/Sort";
+import {PizzaBlock} from "./components/PizzaBlock/PizzaBlock";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories/>
+                        <Sort />
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                    <PizzaBlock/>
+                    <PizzaBlock/>
+                    <PizzaBlock/>
+                    <PizzaBlock/>
+                    <PizzaBlock/>
+                    <PizzaBlock/>
+                    <PizzaBlock/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
