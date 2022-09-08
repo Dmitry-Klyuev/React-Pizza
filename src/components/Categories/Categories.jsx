7cb3fe75bc7bcfd28e1bc0dc52e6b0f8e6a30757
Dmity-kl <i6169696@gmail.com>
2022-09-08 21:17:31 +0300

    refactoring, add grid

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -14,12 +14,6 @@ export const Categories = () => {
                     {el}
                 </li>
                 ))}
-            {/*<li className={active === 0 && 'active'} onClick={()=>setActive(0)}>Все</li>*/}
-            {/*<li className={active === 1 && 'active'} onClick={()=>setActive(1)}>Мясные</li>*/}
-            {/*<li className={active === 2 && 'active'} onClick={()=>setActive(2)}>Вегетарианская</li>*/}
-            {/*<li className={active === 3 && 'active'} onClick={()=>setActive(3)}>Гриль</li>*/}
-            {/*<li className={active === 4 && 'active'} onClick={()=>setActive(4)}>Острые</li>*/}
-            {/*<li className={active === 5 && 'active'} onClick={()=>setActive(5)}>Закрытые</li>*/}
         </ul>
     </div>;
 };
