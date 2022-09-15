b2d86b24f13dc8fa60fb7f489ee49680a745500b
Dmity-kl <i6169696@gmail.com>
2022-09-15 13:47:59 +0300

    add sort with backend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,14 @@ import PizzaSkeleton from "../components/PizzaBlock/PizzaSkeleton";
 import {PizzaBlock} from "../components/PizzaBlock/PizzaBlock";
 
 
-export const Home = () => {
+export const Home = ({searchValue}) => {
     const [items, setItems] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(true);
     const [activeCategories, setActiveCategories] = React.useState(0);
     const [activeSort, setActiveSort] = React.useState('популярности ↓');
 
     const sort = ['популярности ↓', 'популярности ↑', 'цене ↓' ,'цене ↑', 'алфавиту ↓' , 'алфавиту ↑'];
-    const sortCategory = activeCategories > 0 ? activeCategories : '';
+    const sortCategory = activeCategories > 0 ? `category=${activeCategories}` : '';
 
     let sortBy = ''
     switch (activeSort) {
@@ -28,16 +28,17 @@ export const Home = () => {
             break
         case 'алфавиту ↑' : sortBy = 'sortBy=title&order=desc'
     }
+    const search = searchValue ? `search=${searchValue}` : '';
 
     React.useEffect(() => {
         setIsLoading(true);
-        fetch(`https://6314e211fa82b738f7501ac3.mockapi.io/items?${sortBy}&category=${sortCategory}`)
+        fetch(`https://6314e211fa82b738f7501ac3.mockapi.io/items?${sortBy}&${search}&${sortCategory}`)
             .then(res => res.json())
             .then((data) => {
                 setItems(data);
                 setIsLoading(false);
             });
-    }, [activeSort, activeCategories]);
+    }, [activeSort, activeCategories, searchValue]);
 
     return (
         <>
